eb1a0db450021d2875123f83be1438b18955aa69
osc_19528380 <9701944+zxy921666@user.noreply.gitee.com>
2026-03-22 19:52:38 +0800

    初始提交：软光栅模块

--- h1.cpp
+++ h1.cpp
@@ -16,20 +16,21 @@ struct PhongShader : IShader {
     vec4 pos[3];
 
     PhongShader(const vec3& l, const Model& m) : model(m) {
-        light = normalized(ModelView * vec4{ l.x, l.y, l.z, 0. });
-        normalMat = ModelView.invert_transpose();
+        light = normalize(ModelView * vec4{ l.x, l.y, l.z, 0. });
+        normalMat = inverse(ModelView);
     }
 
     vec4 vertex(int f, int v) {
         uv[v] = model.uv(f, v);
-        nrm[v] = normalized(normalMat * model.normal(f, v));
+        nrm[v] = normalize(normalMat * model.normal(f, v));
         pos[v] = ModelView * model.vert(f, v);
         return Perspective * pos[v];
     }
 
     std::pair<bool, TGAColor> fragment(const vec3 bar) const override {
         vec2 st = uv[0] * bar[0] + uv[1] * bar[1] + uv[2] * bar[2];
-        vec4 n = normalized(nrm[0] * bar[0] + nrm[1] * bar[1] + nrm[2] * bar[2]);
+        vec4 n = normalize(nrm[0] * bar[0] + nrm[1] * bar[1] + nrm[2] * bar[2]);
+        vec4 fragPos = pos[0] * bar[0] + pos[1] * bar[1] + pos[2] * bar[2];
 
         vec4 e1 = pos[1] - pos[0];
         vec4 e2 = pos[2] - pos[0];
@@ -38,25 +39,28 @@ struct PhongShader : IShader {
 
         double inv = 1.0 / (d1.x * d2.y - d2.x * d1.y);
 
-        vec4 t = normalized((e1 * d2.y - e2 * d1.y) * inv);
-        vec4 b = normalized((e2 * d1.x - e1 * d2.x) * inv);
+        vec4 t = normalize((e1 * d2.y - e2 * d1.y) * inv);
+        vec4 b = normalize((e2 * d1.x - e1 * d2.x) * inv);
 
         mat<4, 4> TBN = { t, b, n, {0,0,0,1} };
 
         vec4 mapN = model.normal(st);
-        vec4 N = normalized(TBN.transpose() * mapN);
+        vec4 N = normalize(TBN.transpose() * mapN);
 
-        double diff = std::max(0., N * light);
-        vec4 R = normalized(N * (2.0 * (N * light)) - light);
+        vec4 L = normalize(light);        
+        vec4 V = normalize(fragPos * -1);      // 视线方向（假设相机在原点）
 
-        double spec = std::pow(std::max(R.z, 0.), 32.0) *
+        // Blinn-Phong 高光
+        vec4 H = normalize(L + V);         
+        double diff = std::max(0., N * L);
+        double spec = std::pow(std::max(0., N * H), 32.0) * 
             (sample2D(model.specular(), st)[0] / 255.0);
+
 
         double amb = 0.2;
 
         TGAColor col = sample2D(model.diffuse(), st);
-        TGAColor glow = glowSample2D(model.glow(), st, 40);
-
+        TGAColor glow = glowSample2D(model.glow(), st, 40);      
         double inten = amb + diff + spec;
 
         for (int i = 0; i < 3; i++) {
